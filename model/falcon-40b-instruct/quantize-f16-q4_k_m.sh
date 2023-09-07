12fe0a5d8b7a2a934b25d8b2b8611d7442e2dc23
Joe Spizzandre <104811936+JoeSpizz@users.noreply.github.com>
2023-09-07 09:58:34 -0400

    Update quantize-f16-q4_k_m.sh

--- model/falcon-40b-instruct/quantize-f16-q4_k_m.sh
+++ model/falcon-40b-instruct/quantize-f16-q4_k_m.sh
@@ -1,8 +1,11 @@
+# This script expects a f16 base model to quantize
+# Actually perform the quantization
 ../../git/llama.cpp/quantize \
     model/ggml-model-f16.gguf \
     model/falcon-40b-instruct.gguf.q4_k_m.bin \
     q4_k_m
-
+    
+# Test quantization efficacy by running a prompt. Success will result in response tokens being generated
 ../../git/llama.cpp/main -ngl 100 --threads 21 \
     --model model/falcon-40b-instruct.gguf.q4_k_m.bin \
     --file ../../prompt/instruct/ansible.txt
