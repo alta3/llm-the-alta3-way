dda95a7141fa25858509ddf6e40c1c578b84fea5
seaneon <35880398+seaneon@users.noreply.github.com>
2023-09-07 16:55:37 -0400

    Update test.sh

--- model/Llama-2-70B-Orca-200k/test.sh
+++ model/Llama-2-70B-Orca-200k/test.sh
@@ -1,1 +1,10 @@
-echo "WRITE ME PLZKTHX"
+#!/bin/bash
+
+# change into this script's dir
+cd -P -- "$(dirname -- "$0")"
+
+# Run inference with the converted model `llama.cpp`
+# test with low cpu numbers, no gpu, 10 tokens in order to get numbers
+../../git/llama.cpp/main -ngl 0 --threads 8 --n-predict 10 \
+    --model model/llama-2-70b-orca-200k.Q2_K.gguf \
+    --file ../../prompt/instruct/ansible.txt
