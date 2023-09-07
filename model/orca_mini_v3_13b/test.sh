20d62d6e7fd99a8e2d61346d7032871c912646eb
Joe Spizzandre <104811936+JoeSpizz@users.noreply.github.com>
2023-09-07 09:22:49 -0400

    Update test.sh

--- model/orca_mini_v3_13b/test.sh
+++ model/orca_mini_v3_13b/test.sh
@@ -12,7 +12,7 @@ python3 -m pip install numpy
     --output model/orca_mini_v3_13b.gguf.q8_0.bin
 
 # Run inference with the converted model `llama.cpp`
-# test with low cpu numbers, no gpu, 10 tokens
+# test with low cpu numbers, no gpu, 10 tokens in order to get numbers
 ../../git/llama.cpp/main -ngl 0 --threads 8 --n-predict 10 \
     --model model/orca_mini_v3_13b.gguf.q8_0.bin \
     --file ../../prompt/instruct/ansible.txt
