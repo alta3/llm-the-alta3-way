9d5b87772d3b5ef0c005f7aace7d111912f56099
BicycleWalrus <32198667+BicycleWalrus@users.noreply.github.com>
2023-09-18 22:30:53 -0400

    Made it better

--- model/infinite_shakespeare/mini-gpt-intro-2.py
+++ model/infinite_shakespeare/mini-gpt-intro-2.py
@@ -1,7 +1,11 @@
-# here are all the unique characters that occur in this text --This provides the library of all possible Tokens
-# The following code uses set() to extract all UNIQUE characters from the input.txt, list() puts those
+# Open the file for reading 
+with open('input.txt', 'r', encoding='utf-8') as f:
+  text = f.read()
 
-characters into a single list, and sorted() sorts the list. 
+# here are all the unique characters that occur in this text --This provides the library of all possible Tokens
+# The following code uses set() to extract all UNIQUE characters from the input.txt, list() puts those 
+# characters into a single list, and sorted() sorts the list.
+
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
 print(''.join(chars))
