395cddc345fbde3ae38b55874dd7faf74441caea
BicycleWalrus <32198667+BicycleWalrus@users.noreply.github.com>
2023-09-18 22:22:19 -0400

    I made it better

--- model/infinite_shakespeare/mini-gpt-intro.py
+++ model/infinite_shakespeare/mini-gpt-intro.py
@@ -1,8 +1,8 @@
-# read it in to inspect it
+# Open the file for reading 
 with open('input.txt', 'r', encoding='utf-8') as f:
   text = f.read()
 
 print("length of dataset in characters: ", len(text))
 
-# let's look at the first 1000 characters
+# Print the first 1000 characters
 print(text[:1000])
