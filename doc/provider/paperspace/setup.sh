52278e60a3295af1ceb1e877598928bcefb340e9
Joe Spizzandre <104811936+JoeSpizz@users.noreply.github.com>
2024-07-11 09:57:41 -0400

    Update setup.sh

--- doc/provider/paperspace/setup.sh
+++ doc/provider/paperspace/setup.sh
@@ -1,3 +1,7 @@
+Acces the machine with 
+`ssh paperspace@a100-1.alta3.training`
+Assuming the routing in AWS hasn't changed. 
+
 {
   sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
   sudo sed -i 's/%sudo\sALL=(ALL:ALL) ALL/%sudo   ALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers
